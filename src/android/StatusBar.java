d248d504a42c8e7ed0e9dbe5c7715f7f8fc62c4b
Adam Szmyd <szmydadam@gmail.com>
2018-07-18 14:15:31 +0200

    Fix local attribute accessing and indentation

--- src/android/StatusBar.java
+++ src/android/StatusBar.java
@@ -103,7 +103,7 @@ public class StatusBar extends CordovaPlugin {
                     // use KitKat here to be aligned with "Fullscreen"  preference
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                         int uiOptions = window.getDecorView().getSystemUiVisibility();
-                        if (transparent) {
+                        if (this.transparent) {
                             uiOptions |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
                         } else {
                             uiOptions &= ~View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
@@ -250,21 +250,21 @@ public class StatusBar extends CordovaPlugin {
 
     private void setStatusBarTransparent(final boolean transparent) {
         this.transparent = transparent;
-            final Window window = cordova.getActivity().getWindow();
-            if (transparent) {
-                window.getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        final Window window = cordova.getActivity().getWindow();
+        if (transparent) {
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
-                if (Build.VERSION.SDK_INT >= 21) {
-                    window.setStatusBarColor(Color.TRANSPARENT);
-                }
+            if (Build.VERSION.SDK_INT >= 21) {
+                window.setStatusBarColor(Color.TRANSPARENT);
             }
-            else {
-                window.getDecorView().setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                | View.SYSTEM_UI_FLAG_VISIBLE);
-            }
+        }
+        else {
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_VISIBLE);
+        }
     }
 
     private void setStatusBarStyle(final String style) {
